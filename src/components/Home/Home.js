4a1e83b41dfba3469c94e3f1043dd946170c8712
Banks <Russell.Banks@kyocera.com>
2023-04-05 12:19:43 -0700

    idk

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,10 +8,9 @@ function Home() {
     <NavBar />
     <div className='body'>
     <div className='content'>
-        <h1 className='h1'>Welcome to my personal website!</h1>
-        <p className='p'>Thanks for stopping by. My name is [Your Name], and this is where you can find out more about me, my work, and my interests.</p>
-        <p className='p'>Feel free to explore the site, check out my portfolio, or read my latest blog posts. If you have any questions or want to get in touch, don't hesitate to reach out using the contact form.</p>
-        <p className='p'>Thanks again for visiting, and I hope you enjoy your stay!</p>
+        <h1 className='h1'>Welcome!</h1>
+        <p className='p'>Hello! My name is Russell Banks, a Computer Science Student at University of California, San Diego</p>
+        <p className='p'>This is a website I have built as a personal portfolio, in order to showcase projects, experience, and myself</p>
     </div>
     </div>
     </div>
